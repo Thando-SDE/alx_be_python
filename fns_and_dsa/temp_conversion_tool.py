e3961906ad5497cd825748da5d5208b860584907
Thando-SDE <ThandoMjacu@gmail.com>
2025-09-29 00:45:24 +0200

    Fixed "fahrenheit" to "Fahrenheit" in the prompt

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
@@ -14,7 +14,7 @@ def main():
     try:
         temperature = float(input("Enter the temperature to convert: "))
 
-        unit = input("Is this temperature in Celsius or fahrenheit? (C/F): ").upper().strip()
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper().strip()
 
         if unit == 'F':
             converted_temp = convert_to_celsius(temperature)
