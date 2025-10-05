3e224c8fa9de1f75093596ca2ee766c95ec5cebe
Thando-SDE <ThandoMjacu@gmail.com>
2025-10-05 19:35:32 +0200

    Impement Library Management System with Book and Library classes

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,61 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False  # Private attribute
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Mark the book as returned/available."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+    def __str__(self):
+        """String representation of the book."""
+        return f"{self.title} by {self.author}"
+
+
+class Library:
+    def __init__(self):
+        self._books = []  # Private list to store books
+
+    def add_book(self, book):
+        """Add a book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return True
+        return False
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return True
+        return False
+
+    def list_available_books(self):
+        """List all available books."""
+        available_books = [book for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                print(book)
+        else:
+            print("No books available")
