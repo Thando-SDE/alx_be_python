c65dabd5ad6980ba298dc830e2784a2f5ee52d7c
Thando-SDE <ThandoMjacu@gmail.com>
2025-10-12 19:22:34 +0200

    rename main.py to main_book_class.py

---
+++ oop/library_system.py
@@ -0,0 +1,36 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.auhtor = author
+
+    def get_details(self):
+        return f"book: {self.title} by {self.author}"
+    
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def get_details(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+    
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def get_details(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+    
+class Library:
+    def __init__(self):
+        self.books = []
+    def add_book(self, book):
+        self.books.append(book)
+        print(f"Added: {book.title}")
+    def list_books(self):
+        print("\n--- Library Collection ---")
+        for book in self.books:
+            print(book.get_details())
+            print("-----------------------")
+    
